15c30f5a56d6d4126c928a66af33aaef95a9c00b
zhangjiahui <2319671152@qq.com>
2025-03-04 14:30:39 +0800

    init: 初始化

---
+++ packages/cli/dist/index.js
@@ -0,0 +1,5 @@
+#! /usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@kg-cli/core");
+console.log((0, core_1.add)(1, 2));
---
+++ packages/cli/index.ts
@@ -0,0 +1,5 @@
+#! /usr/bin/env node
+
+import { add } from "@kg-cli/core";
+
+console.log(add(1, 2));
